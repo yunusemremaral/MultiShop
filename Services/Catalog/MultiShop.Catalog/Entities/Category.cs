7f0b41010a6846754fb2e39a2d87216ffdeee8b6
Yunus Emre Maral <maralyunus41@gmail.com>
2024-10-03 11:35:49 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] //Mongodbde böyle tutuluyor id ler string ve özel 
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        
+    }
+}
