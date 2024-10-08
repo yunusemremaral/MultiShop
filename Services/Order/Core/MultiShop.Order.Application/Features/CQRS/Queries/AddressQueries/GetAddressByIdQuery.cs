e24f56c1f7ab09f3424ead39eb6f52c773d51c00
Yunus Emre Maral <maralyunus41@gmail.com>
2024-10-08 12:31:50 +0300

    Order CQRS geneli bitti . Sırada Mediator  var ...

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Queries/AddressQueries/GetAddressByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries
+{
+    public class GetAddressByIdQuery
+    {
+        public int Id { get; set; }
+        public GetAddressByIdQuery(int id) // constructer ile dönüyoruz
+        {
+            Id = id;
+        }
+    }
+
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IRepository<T>  where T : class
+    {
+        Task<List<T>> GetAllAsync();   // genel işlemler
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T Entity);
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+        Task GetByIdAsync(GetAddressByIdQuery query);
+    }
+}
