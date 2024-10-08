fd5d5b2bffc9817164dce3e8ebbcea599e8b5a30
Yunus Emre Maral <maralyunus41@gmail.com>
2024-10-08 13:10:44 +0300

    MediatR   ordering kısmında kullanıldı ve bağlandı ...

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrderingsController(IMediator mediator) // Tek tek cqrs deki gibi constructerlar dönmüyoruz kendisi bunu yapıyor ...
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());   // send ile irequest  miras aldığımız  sınıfı cagırıyoruz ...
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            var values = await _mediator.Send(new GetOrderingByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering(CreateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Sipariş başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveOrdering(int id)
+        {
+            await _mediator.Send(new RemoveOrderingCommand(id));
+            return Ok("Sipariş başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Sipariş başarıyla güncellendi");
+        }
+
+        
+
+    }
+}
