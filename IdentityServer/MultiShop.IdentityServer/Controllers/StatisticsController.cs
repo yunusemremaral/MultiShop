6ab668e54567e8ce105672f83e7d74aa7fbeeadb
Yunus Emre Maral <maralyunus41@gmail.com>
2024-10-21 15:34:03 +0300

    Identity Server Kuruldu tablolar hazırlanıp örnek veri girişi yapıldı ...

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/StatisticsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+    }
+}
