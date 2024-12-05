25b1fb6b9b2917b3f21e5b61488309a25e450b71
Yunus Emre Maral <maralyunus41@gmail.com>
2024-12-05 19:49:18 +0300

    Category consume işlemlerinin tamamlanması .

---
+++ Frontends/MultiShot.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShot.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutHeadComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShot.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShot.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
