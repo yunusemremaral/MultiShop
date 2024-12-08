3d1685e277112491abf3629ec5318a3470f09b21
Yunus Emre Maral <maralyunus41@gmail.com>
2024-12-08 13:08:15 +0300

    Comment Mikroservisinin oluşturulup Admin ve UI tarafına consume edilmesi ...

--- Frontends/MultiShot.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShot.WebUI/Controllers/ProductListController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDto;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShot.WebUI.Controllers
 {
@@ -18,6 +21,31 @@ namespace MultiShot.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "test";
+            createCommentDto.Rating = 1;
+            createCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "65dc67a7705038bfa8fb1f87";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7241/api/Comments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
